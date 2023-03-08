88eb0be842dbf363580726d7db19e004b2a9203b
alvaros86 <114369683+alvaros86@users.noreply.github.com>
2023-03-08 20:44:53 -0300

    Add files via upload

---
+++ ejerciciotema4.java
@@ -0,0 +1,14 @@
+package com.sentenciasdecontrol;
+
+public class ejerciciotema4 {
+
+    public static void main(String[] args) {
+        String numeroIf = "0";
+
+        if (numeroIf == "0")
+            System.out.println("Es positivo");
+         else
+            System.out.println("Es negativo");
+
+    }
+}
